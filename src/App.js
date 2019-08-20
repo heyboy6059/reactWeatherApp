8919084654eac7c189627a100c5953bd5c5565d8
Daniel <heyboy6059@gmail.com>
2019-08-20 22:04:39 +1000

    handeld blank forms & implement stateless functional component as an alternative

--- src/App.js
+++ src/App.js
@@ -23,16 +23,24 @@ class App extends React.Component {
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     
     const data = await api_call.json();
+    
+    if (city && country){
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error:""
+      })
+    } else {
+      this.setState({
+        error:"Please type the city and country"
+      })
+    }
     console.log(data);
   
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      erro:""
-    })
+
   }
 
   render() {
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -8,9 +8,27 @@ render(){
                 {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
                 {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
                 {this.props.description && <p>Conditions: {this.props.description}</p>}
+                {this.props.error && <p>{this.props.error}</p>}
             </div>
         );
     }
 };
 
 export default Weather;
+
+
+
+
+// stateless functional components
+
+// const Weather = () => {
+//     return (
+//         <div>
+//         {this.props.city && this.props.country && <p>Location:{this.props.city}, {this.props.country}</p>}
+//         {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+//         {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+//         {this.props.description && <p>Conditions: {this.props.description}</p>}
+//         {this.props.error && <p>{this.props.error}</p>}
+//     </div>
+//     );
+// }
