266740c6f507fd61d7e87f0828cb7558cdb5a923
Daniel <heyboy6059@gmail.com>
2019-08-20 13:30:06 +1000

    Added basic App component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hi</p>
+        <h1>Ho</h1>
+      </div>
+
+    );
+  }
+}
+
+export default App;
