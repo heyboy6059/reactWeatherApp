ddb27d06209f3d22a974244533b0aed16e80025c
Daniel <heyboy6059@gmail.com>
2019-08-20 14:04:25 +1000

    Added API_KEY from openweather and getWeatherMethod

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const API_KEY = "e22aac4add5291067b43ea666c6778af";
+
 class App extends React.Component {
+
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
+    
+    const data = await api_call.json();
+    console.log(data);
+  
+  }
+
   render() {
     return (
       <div>
